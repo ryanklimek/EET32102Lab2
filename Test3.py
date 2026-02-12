aef85ef83453f9e67e997d8f029745df7fba41b1
hpg3 <hpg3@pct.edu>
2026-02-12 10:29:00 -0500

    Getting actual measured value through calculations

--- Test3.py
+++ Test3.py
@@ -36,7 +36,7 @@ for device in resources:
 supply.write("OUTPut CH1,ON")
 time.sleep(2) 
 amps = [0.01, 0.1, 1] 
-meas_value = [] 
+meas_res = []
 num_wires = 5 
  
 supply.write("CH1:VOLT 1") 
@@ -44,11 +44,18 @@ for num in range(num_wires):
     i = 0 
     for run in range(3): 
         string = "CH1:CURRent " + str(amps[i]) 
-        supply.write(string) 
-        value = dmm.query("MEAS:VOLT:DC?") 
-        meas_value.append(value) 
-        i +=1 
-        time.sleep(120) 
+        supply.write(string)
+        cur = float(supply.query("CH1:CURR?"))
+        print(cur)
+        time.sleep(1)
+        volt = float(dmm.query("MEAS:VOLT:DC?"))
+        time.sleep(1)
+        print(volt)
+        res = volt/cur
+        meas_res.append(res)
+        i+=1
+        time.sleep(120)
+    meas_value.append('\n')
     if num < num_wires: 
         print("Wire Done. Set up next wire.\n") 
         input("Press ENTER when next wire is set up.") 
@@ -57,3 +64,9 @@ for num in range(num_wires):
         print("All wires complete.\n") 
  
 supply.write("OUTPut CH1,OFF") 
+
+with open('Gabloff_EET321_Lab3_Test3.txt', 'a') as results:
+    results.write('Detected Waveforms:\n')
+    for value in meas_value:
+        results.write(value)
+    results.write('\n\n')
